35a7d253d22747f1dfa8080aaabe565fc0f610e6
Taha ahmad <tahahmad01@gmail.com>
2024-09-28 21:29:47 +0500

    upload

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// <!-- Initialize Swiper -->
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 50,
+      },
+    },
+  });
